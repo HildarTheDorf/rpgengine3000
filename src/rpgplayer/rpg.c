4f5f1a07f404dbadfa72351e6f3550ac1941e1a1
xv8m4g3r <xv8m4g3r@gmail.com>
2012-12-18 13:52:17 +0000

    Makefile done.

--- src/rpgplayer/rpg.c
+++ src/rpgplayer/rpg.c
@@ -4,7 +4,7 @@ static FILE * init(char*);
 
 int main(int argc, char *argv[])
 {
-    printf("This game uses rpgengine3000 version %.1f\nThis product is free software released under the GPL v3\nFor more information see the file COPYING\n\n", VERSION);
+    printf("rpgengine3000 version %.1f\nThis product is free software released under the GPL v3\nFor more information see the file COPYING\n\n", VERSION);
 
     if (argc != 2) {
         printf("Error: insufficent arguments\nUsage: %s gamefile\n", argv[0]);
